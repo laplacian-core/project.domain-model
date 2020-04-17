5798044038b61e7a05088084a3f183aa180c922f
iwauo <iwauo.tajima@gmail.com>
2020-04-17 12:37:49 +0900

    update project base template

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -48,7 +48,7 @@ file_list() {
 #
 generate() {
   ${SCRIPT_BASE_DIR}/laplacian-generate.sh \
-    --schema 'laplacian:laplacian.schema.metamodel:1.0.0' \
+    --plugin 'laplacian:laplacian.schema.metamodel:1.0.0' \
     --template 'laplacian:laplacian.project-base.template:1.0.0' \
     --template 'laplacian:laplacian.schema-doc.template:1.0.0' \
     --model 'laplacian:laplacian.project.schema-model:1.0.0' \
